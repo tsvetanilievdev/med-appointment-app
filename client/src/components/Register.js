d2359c57f781534db02b46202b6f6cbcdea6a11c
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-06-09 14:15:54 +0300

    Refactor Login component - pure css

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,65 +1,53 @@
 import React from 'react';
 import { Button, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
 function Register() {
     return (
         <div className="authentication">
-            <div className="authentication-form card p-3">
-                <h1 className="card-title">Nice to meet you</h1>
-                <Form layout="vertical">
-                    <Form.Item
-                        label="Name"
-                        name="name"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your name',
-                            },
-                        ]}
-                    >
-                        <Input
-                            className="override"
-                            placeholder="Enter your name"
-                        />
-                    </Form.Item>
 
-                    <Form.Item
-                        label="Username"
-                        name="username"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your username',
-                            },
-                        ]}
-                    >
-                        <Input
-                            className="override"
-                            placeholder="Enter your username"
-                        />
-                    </Form.Item>
+            <div className='authentication-form card'>
+                    <h1 className='card-title'>Nice to meet you</h1>
+                <form className='vertical2' onSubmit={onSubmitRegister}>
+                    <div className='form-item'>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name='name' id='name' placeholder='Enter your name' />
+                    </div>
 
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your password',
-                            },
-                        ]}
-                    >
-                        <Input.Password style={{ className: 'override' }} />
-                    </Form.Item>
+                    <div className='form-item'>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" name='username' id='username' placeholder='Enter your username' />
+                    </div>
 
-                    <Form.Item>
-                        <Button type="primary primary-button" htmlType="submit">
-                            Submit
-                        </Button>
-                    </Form.Item>
-                </Form>
+                    <div className='form-item'>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" name='password' id='password' placeholder='Enter your password' />
+                    </div>
+                    
+                    <div className='form-item'>
+                        <label htmlFor="repass">Repeat Password:</label>
+                        <input type="password" name='repass' id='repass' placeholder='Enter your password' />
+                    </div>
+
+
+                    <div className='form-item'>
+                        <button className='primary-button'>Submit</button>
+                    </div>
+
+                    <div className='form-item'>
+                        <span>Already registered?</span>
+                        <Link to='/login' className='anchor'>Login</Link>
+                    </div>       
+                </form>
             </div>
         </div>
     );
 }
 
 export default Register;
+
+function onSubmitRegister(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    console.log(formData);
+    console.log(Object.fromEntries(formData));
+}
