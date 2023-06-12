3a8dacfbbb1f5ea449d8b92583a3307db8712f6e
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-06-12 13:51:13 +0300

    install bcrypt

---
+++ server/routes/userRoute.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+router.post('/login', async (req, res) => {
+
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ require('./config/dbConfig');
 const express = require('express');
 
 const app = express();
-
+const userRoute = require('./routes/userRoute');
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use('/api/user', userRoute);
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
